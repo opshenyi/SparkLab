a4a55a8ffdc113cab7822d16d8c4f4180abe7609
wanglin <devvanglin@gmail.com>
2026-06-10 00:46:49 +0800

    Remove global update notifier

--- web/src/components/Providers.tsx
+++ web/src/components/Providers.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 import { ThemeProvider } from '@/contexts/ThemeContext';
-import UpdateNotifier from './UpdateNotifier';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider>
       {children}
-      <UpdateNotifier />
     </ThemeProvider>
   );
 }
